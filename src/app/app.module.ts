6a0c8f543247eb2d1a747cc349d6ea35470cf53a
Tolga Halat <gtolgahalat@gmail.com>
2023-12-11 01:34:58 +0300

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { QRCodeModule } from 'angularx-qrcode';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    QRCodeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
